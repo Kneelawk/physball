3e87287498a223c99edf5074edc3a759ffbca4f8
Cyan Kneelawk <kneelawk@gmail.com>
2025-10-08 21:53:50 -0700

    Begin on splash animation

--- src/game/startup/mod.rs
+++ src/game/startup/mod.rs
@@ -1,4 +1,5 @@
 use crate::game::state::GameState;
+use bevy::animation::{animated_field, AnimationTargetId};
 use bevy::asset::embedded_asset;
 use bevy::prelude::*;
 
@@ -13,10 +14,25 @@ impl Plugin for BallphysStartup {
     }
 }
 
-fn splash_screen(
+#[derive(Debug, Clone, Resource, Reflect)]
+struct SplashScreenData {
+    name: Name,
+    target_id: AnimationTargetId,
+    graph: Handle<AnimationGraph>,
+    node_index: AnimationNodeIndex,
+}
+
+fn splash_screen_setup(
     mut cmd: Commands,
-    asset_server: Res<AssetServer>,
     mut animations: ResMut<Assets<AnimationClip>>,
     mut graphs: ResMut<Assets<AnimationGraph>>,
 ) {
+    let name = Name::new("bevy_logo_splash");
+    let target_id = AnimationTargetId::from_name(&name);
+
+    let mut clip = AnimationClip::default();
+
+    clip.add_curve_to_target(target_id, AnimatableCurve::new(animated_field!()))
 }
+
+fn splash_screen(mut cmd: Commands, asset_server: Res<AssetServer>) {}
