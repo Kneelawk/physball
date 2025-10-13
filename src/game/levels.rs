c544e43ee87e699bd6a562d03a24fa0fb802e094
Cyan Kneelawk <kneelawk@gmail.com>
2025-10-12 23:22:58 -0700

    Improve levels slightly

--- src/game/levels.rs
+++ src/game/levels.rs
@@ -2,6 +2,7 @@ use crate::game::state::AppState;
 use crate::type_expr;
 use avian3d::prelude::*;
 use bevy::prelude::*;
+use std::f32::consts::PI;
 
 #[derive(Debug, Default, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
 pub struct LevelsPlugin;
@@ -41,7 +42,7 @@ pub struct PlayerSpawnPoint;
 fn spawn_level(cmd: Commands, level: Res<SelectedLevel>, asset_server: Res<AssetServer>) {
     match *level {
         SelectedLevel::Level1 => spawn_level1(cmd, &asset_server),
-        SelectedLevel::Level2 => {}
+        SelectedLevel::Level2 => spawn_level2(cmd, &asset_server),
     }
 }
 
@@ -56,6 +57,49 @@ fn spawn_level1(mut cmd: Commands, asset_server: &AssetServer) {
             Collider::cuboid(10.0, 0.2, 10.0),
             Transform::from_xyz(0.0, -0.1, 0.0)
         )],
+    ));
+
+    cmd.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::OVERCAST_DAY,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_rotation_x(-PI / 4.0) * Quat::from_rotation_z(-PI / 6.0)),
+    ));
+
+    cmd.trigger(LevelReadyEvent);
+}
+
+fn spawn_level2(mut cmd: Commands, asset_server: &AssetServer) {
+    cmd.spawn((
+        LevelObject,
+        Transform::default(),
+        Mesh3d(asset_server.add(Plane3d::new(Vec3::Y, Vec2::splat(5.0)).into())),
+        MeshMaterial3d(asset_server.add(type_expr!(StandardMaterial, Color::WHITE.into()))),
+        children![(
+            RigidBody::Static,
+            Collider::cuboid(10.0, 0.2, 10.0),
+            Transform::from_xyz(0.0, -0.1, 0.0)
+        )],
+    ));
+
+    cmd.spawn((
+        LevelObject,
+        Transform::from_xyz(0.0, 0.25, -4.5),
+        Mesh3d(asset_server.add(Cuboid::from_length(0.5).into())),
+        MeshMaterial3d(asset_server.add(type_expr!(StandardMaterial, Color::WHITE.into()))),
+        RigidBody::Dynamic,
+        Collider::cuboid(0.5, 0.5, 0.5),
+    ));
+
+    cmd.spawn((
+        DirectionalLight {
+            illuminance: light_consts::lux::OVERCAST_DAY,
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_rotation(Quat::from_rotation_z(-PI / 6.0) * Quat::from_rotation_x(-PI / 4.0)),
     ));
 
     cmd.trigger(LevelReadyEvent);
