e124e7f60f1a418acda6228a68ea8db0f19969b8
Cyan Kneelawk <kneelawk@gmail.com>
2025-10-18 18:08:11 -0700

    Clean up logging

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -11,6 +11,7 @@ use avian3d::PhysicsPlugins;
 use bevy::app::plugin_group;
 use bevy::input_focus::InputDispatchPlugin;
 use bevy::input_focus::tab_navigation::TabNavigationPlugin;
+use bevy::log::LogPlugin;
 use bevy::prelude::*;
 use bevy::ui_widgets::UiWidgetsPlugins;
 use bevy_svg::SvgPlugin;
@@ -18,14 +19,43 @@ use bevy_svg::SvgPlugin;
 pub fn physball_client_main() -> AppExit {
     App::new()
         .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    resizable: false,
-                    resolution: (1280, 720).into(),
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resizable: false,
+                        resolution: (1280, 720).into(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(LogPlugin {
+                    filter: format!(
+                        concat!(
+                            "{default},",
+                            "symphonia_bundle_mp3::demuxer=warn,",
+                            "symphonia_format_caf::demuxer=warn,",
+                            "symphonia_format_isompf4::demuxer=warn,",
+                            "symphonia_format_mkv::demuxer=warn,",
+                            "symphonia_format_ogg::demuxer=warn,",
+                            "symphonia_format_riff::demuxer=warn,",
+                            "symphonia_format_wav::demuxer=warn,",
+                            "calloop::loop_logic=error,",
+                            "avian3d::dynamics::solver::islands::sleeping=error,",
+                        ),
+                        default = bevy::log::DEFAULT_FILTER
+                    ),
+                    fmt_layer: |_| {
+                        Some(Box::new(
+                            bevy::log::tracing_subscriber::fmt::Layer::default()
+                                .without_time()
+                                .map_fmt_fields(
+                                    bevy::log::tracing_subscriber::field::MakeExt::debug_alt,
+                                )
+                                .with_writer(std::io::stderr),
+                        ))
+                    },
                     ..default()
                 }),
-                ..default()
-            }),
             PhysicsPlugins::default(),
             SvgPlugin,
             BallphysClient,
