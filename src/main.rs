2496e01d8b87e1b018b412a0652ad1f242af6e75
Cyan Kneelawk <kneelawk@gmail.com>
2025-11-09 21:32:39 -0800

    Improve logging

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ mod game;
 mod macros;
 
 fn main() -> ExitCode {
+    #[cfg(target_arch = "wasm32")]
+    {
+        console_error_panic_hook::set_once();
+    }
+
     // maybe use clap later
     game::physball_client_main().report()
 }
